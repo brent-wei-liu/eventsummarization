bb5fb1077d5773981b3656f61590066d8e8b1169
yexijiang <yexijiang@b485859c-ee6e-a96c-703b-91b04664177b>
2010-09-27 04:11:16 +0000

    Finished TotalLength (TL) and LocalLength (LL), the E should be fixed.

---
+++ src/basic/EventManager.java
@@ -0,0 +1,78 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Mange the event, containing the information about how many event in total
+ * and how many distinct event in total.
+ * @author Yexi Jiang <http://sites.google.com/site/yexijiang/>
+ *
+ */
+public class EventManager {
+	private List<Event> eventSequence;	//	event sequence
+	
+	private List<Event> eventSet;
+	
+	public EventManager(){
+		eventSequence = new ArrayList<Event>();
+		eventSet = new ArrayList<Event>();
+	}
+	
+	public void read(String filepath) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(filepath));
+		String line;
+		while((line = reader.readLine()) != null){
+			if(line.length() == 0){
+				continue;
+			}
+			String[] tokens = line.split("\\t");
+			String[] date = tokens[1].split("-");
+			String[] time = tokens[2].split(":");
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(Integer.parseInt(date[0]), Integer.parseInt(date[1]), Integer.parseInt(date[2]),
+						Integer.parseInt(time[0]), Integer.parseInt(time[1]), Integer.parseInt(time[2]));
+			int eventNo = Integer.parseInt(tokens[5]);
+			Event event = new Event(tokens[0], calendar, tokens[3], tokens[4], eventNo, tokens[6], tokens[7]);
+			addEvent(event);
+		}
+	}
+	
+	public void addEvent(Event event){
+		eventSequence.add(event);
+		boolean found = false;
+		for(int i = 0; i < eventSet.size(); ++i){
+			Event e = eventSet.get(i);
+			if(e.equals(event)){
+				e.increaseCount();
+				found = true;
+			}
+		}
+		if(found == false){
+			eventSet.add(event);
+		}
+	}
+	
+	public int distinctEventSize(){
+		return eventSet.size();
+	}
+	
+	public int length(){
+		return eventSequence.size();
+	}
+	
+	public List<Event> getSequence(){
+		return eventSequence;
+	}
+	
+	public List<Event> getEventSet(){
+		return eventSet;
+	}
+	
+}
---
+++ src/basic/Test.java
@@ -0,0 +1,15 @@
+package basic;
+
+import kdd08.CodeCost;
+
+public class Test {
+	public static void main(String[] args) throws Exception{
+		EventManager manager = new EventManager();
+		manager.read("g:/dataset/event/application.txt");
+		System.out.println("Distinct event size:" + manager.distinctEventSize());
+		System.out.println("Event length:" + manager.length());
+		CodeCost codeCost = new CodeCost(manager);
+		double cost = codeCost.optimalTL();
+		System.out.println(cost);
+	}
+}
